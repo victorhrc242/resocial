5053a89db995d414c158bd91deeb39db67d0c37c
victorhrc242 <163910434+victorhrc242@users.noreply.github.com>
2024-12-21 23:11:01 -0300

    fazendo graça versão2

--- Resocial/src/App.vue
+++ Resocial/src/App.vue
@@ -3,6 +3,7 @@
     <div class="wrapper">
       <nav>
         <h1>Oto Gostozo</h1>
+        <H1>Brteno Gostozo</H1>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
